55d80e9dd3b3cd019468ddb3291f52beee3d040e
Mujtaba <macbook@MacBooks-MacBook-Pro.local>
2024-09-20 06:53:11 +0500

    make it better

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request, jsonify, url_for
+import requests
+from requests.auth import HTTPBasicAuth
+
+app = Flask(__name__)
+
+clients = [
+            { 'name': "MVA Client A", "link" : 'https://app.leadconduit.com/flows/661eeb850ebe9b2e4e22ca05/sources/66b2780076bac1b4195cb287/submit'}
+            
+            ]
+
+API_KEY = "3a74fe14e6bd37d4838a237888315178"
+
+ehb_api_url = "https://experthelping.dialerhosting.com/RffsxNwc/non_agent_api.php"
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+
+    
+    if request.method == 'POST':
+        source = request.args.get('source')
+        user = request.args.get('user')
+        password = request.args.get('pass')
+        list_id = request.args.get('list_id')
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        address1 = request.args.get('address1')
+        city = request.args.get('city')
+        postal_code = request.args.get('postal_code')
+        state = request.args.get('state')
+        phone_number = request.args.get('phone_number')
+        created_at = request.args.get('created_at')
+        IP_address = request.args.get('IP_address')
+        Jornaya_token = request.args.get('Jornaya_token')
+        TrustedForm = request.args.get('TrustedForm')
+        URL = request.args.get('URL')
+        custom_fields = request.args.get('custom_fields')
+        subID = request.args.get('subID')
+        email = request.args.get('email')
+
+        data_dict = {
+            "source": source,
+            "user": user,
+            "password": password,
+            "list_id": list_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "address1": address1,
+            "city": city,
+            "postal_code": postal_code,
+            "state": state,
+            "phone_number": phone_number,
+            "created_at": created_at,
+            "IP_address": IP_address,
+            "Jornaya_token": Jornaya_token,
+            "TrustedForm": TrustedForm,
+            "URL": URL,
+            "custom_fields": custom_fields,
+            "subID": subID,
+            "email" : email
+        }
+
+        headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'api-version': '4.0'
+        }
+        data = {
+            "phone": phone_number,
+            "email": email
+        }
+
+        # Make the POST request
+        # print(TrustedForm)
+        # print(data_dict)
+        # print(ehb_api_url)
+
+        # print('----------------------______--------------------_______-----------------------------______--------------------_______-----------------------------')
+        response_tf = requests.post(TrustedForm, json=data, headers=headers, auth=HTTPBasicAuth('API', API_KEY))
+        response_ehb_api = requests.post(ehb_api_url, params=data_dict)
+
+
+        return jsonify({
+        "trustedform_response": {
+            "status_code": response_tf.status_code,
+            "response": response_tf.json() if response_tf.status_code == 200 else response_tf.text
+        },
+        "ehb_api_response": {
+            "status_code": response_ehb_api.status_code,
+            "response": response_ehb_api.text
+        }
+    }), 200
+
+    
+    return jsonify({
+        "status": "This is a post request endpoint",
+    }), 200  # 400 Error
+
+if __name__ == '__main__':
+    app.run(debug=True)
